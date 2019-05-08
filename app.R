20e2441591a6e2826781c1e29fbddad45c31066b
Richard S.J. Tol <r.tol@sussex.ac.uk>
2019-05-08 09:22:25 +0100

    Add files via upload

--- app.R
+++ app.R
@@ -4,86 +4,61 @@ library(shiny)
 
 # Define UI for user input
 ui <- fluidPage(
-  titlePanel("Hit the Paris target: Keep temperature below 2 degrees! (Don't move too far from the data.)"),
+  titlePanel("Compute the social cost of carbon as a function of scenario, impact function, and taste parameters."),
   fluidRow(
     column(3,
+      h3(textOutput("SCC")),
+      radioButtons("scenario", "Scenario",
+                   choices = list("SSP1" = 1, "SSP2" = 2, "SSP3" = 3, "SSP4" = 4, "SSP5" = 5, "Trend" = 6), selected = 6),
+      helpText("Scenarios are decomposed according to the Kaya Identity."),
+      radioButtons("radio", "Impact model",
+           choices = list("Hope (2006)" = 1, "Nordhaus (1991)" = 2, "Nordhaus (2008)" = 3, "Tol (2009)" = 4, "Tol (2019)" = 5, "Weitzman (2012)" = 6),selected = 2),
+      helpText("Different authors have proposed different functions to describe the impact of climate change."),
       sliderInput("ClimSens",
         "Warming per CO2 doubling",
         min = 0.1,
         max = 10,
         value = 3.9),
       helpText("Equilibrium warming for doubling of atmospheric CO2."),
-      sliderInput("PopRate",
-        "Rate population growth falls",
-        min = -0.118,
-        max = 0.079,
-        value = -0.019),
-      helpText("The decline, in percentage points, of the population growth rate."),
-      sliderInput("IncRate",
-        "Income growth",
-        min = -2.6,
-        max = 5.8,
-        value = 1.6),
-      helpText("The growth rate of per capita income, in percentage."),
-      sliderInput("AEEI",
-        "Energy efficiency improvement",
-        min = -2.0,
-        max = 3.6,
-        value = 0.8),
-      helpText("The rate of energy efficiency of improvement, in percentage."),
-      sliderInput("ACEI",
-        "Carbon efficiency improvement",
-        min = -2.9,
-        max = 3.3,
-        value = 0.2),
-      helpText("The rate of decarbonisation of the energy supply, in percentage."),
-      sliderInput("Topt",
-        "Optimal temperature",
-        min = -1.0,
+      sliderInput("eta",
+        "Rate of relative risk aversion",
+        min = 0.0,
+        max = 10.0,
+        step = 0.1,
+        value = 1.0),
+      helpText("Curvature of the utility function."),
+      sliderInput("PRTP",
+        "Pure rate of time preference",
+        min = 0.0,
         max = 5.0,
-        step = 0.01,
-        value = -0.38),
-      helpText("The temperature that is best for humans, difference to pre-industrial."),
-      sliderInput("Benchmark",
-        "Impact of 2.5K warming",
-        min = -10.0,
-        max = 5.0,
-        step = 0.01,
-        value = -1.34),
-      helpText("The welfare impact of 2.5K warming, in percent of income."),
+        step = 0.1,
+        value = 1.0),
+      helpText("The utility discount rate, in percent per year."),
       sliderInput("IncElas",
         "Income elasticity of impact",
         min = -3.5,
         max = 1.0,
-        step = 0.01,
-        value = -1.68),
-      helpText("The change in vulnerability due to a change in income."),
-      sliderInput("DiscRate",
-        "Discount rate",
-        min = 0.0,
-        max = 10.0,
         step = 0.1,
-        value = 5.0),
-      helpText("The consumption discount rate, in percent per year."),
+        value = 0.0),
+      helpText("The change in vulnerability due to a change in income."),
       tags$a(href="https://github.com/rtol/SIAM", "Click here for code and data on GitHub")
     ),
     column(3,
+      plotOutput("plotT"),
       plotOutput("plotP"),
       plotOutput("plotCI"),
-      plotOutput("plotT"),
-      textOutput("SCC")
+      helpText("All results are global totals or averages.")
     ),
     column(3,
+      plotOutput("plotI"),
       plotOutput("plotY"),
       plotOutput("plotE"),
-      plotOutput("plotI"),
-      helpText("All results are either global totals or global averages. circles = observations, lines = projections.")
+      helpText("circles = observations, lines = projections.")
       ),
     column(3,
-      plotOutput("plotEI"),
-      plotOutput("plotC"),
       plotOutput("plotD"),
-      helpText("Likelihood ratio is relative to the best fit to the 1969-2018 observations (1850-2018 for temperature).")
+      plotOutput("plotEI"),
+      plotOutput("plotC")
     )
   )
 )
@@ -96,22 +71,89 @@ server <- function(input, output) {
     source("initialize.R")
     
     RFpar <- input$ClimSens/l2/log(2)
-    RFpar0 <- 3.9/l2/log(2)
     popgr <- dpop[NHistYear]
-    addpop <- input$PopRate/100
-    ppop <- matrix(exp(-0.5*((addpop+0.00019)/0.00033)^2),NYear,1)
+    if (input$scenario == "1"){
+      addpop <- -0.000285
+      incgr <- 0.0239
+      eigr <- -0.0192
+      cigr <- -0.0058
+    }
+    else if(input$scenario == "2"){
+      addpop <- -0.000208
+      incgr <- 0.0203
+      eigr <- -0.0131
+      cigr <- -0.0022
+    }
+    else if(input$scenario == "3"){
+      addpop <- -0.000110
+      incgr <- 0.0089
+      eigr <- -0.0053
+      cigr <- -0.0003
+      
+    }
+    else if(input$scenario == "4"){
+      addpop <- -0.000200
+      incgr <- 0.0152
+      eigr <- -0.0199
+      cigr <- -0.0059
+    }
+    else if(input$scenario == "5"){
+      addpop <- -0.000265
+      incgr <- 0.0299
+      eigr <- -0.0165
+      cigr <- -0.0008
+    }
+    else{
+      addpop <- -0.00011
+      incgr <- 0.016
+      eigr <- -0.008
+      cigr <- -0.002
+    }
     
-    incgr <- input$IncRate/100
-    pinc <- matrix(exp(-0.5*((incgr-0.016)/0.014)^2),NYear,1)
     
-    eigr <- -input$AEEI/100
-    pei <- matrix(exp(-0.5*((eigr+0.008)/0.009)^2),NYear,1)
-    
-    cigr <- -input$ACEI/100
-    pci <- matrix(exp(-0.5*((cigr+0.002)/0.010)^2),NYear,1)
-    
-    beta <- input$Benchmark/(2.5*2.5-2*2.5*input$Topt)
-    alpha <- -input$Topt*2*beta
+    if (input$radio == "1") {
+      imp1 <- 0
+      imp13 <- -1.0/2.5^1.3
+      imp2 <- 0
+      imp6 <- 0
+    }
+    else if (input$radio == "2") {
+      imp1 <- 0
+      imp13 <- 0
+      imp2 <- -1.0/3/3
+      imp6 <- 0
+    }
+    else if (input$radio == "3") {
+      imp1 <- 0
+      imp13 <- 0
+      imp2 <- -2.5/3/3
+      imp6 <- 0
+    }
+    else if (input$radio == "4"){
+      imp1 <- 2.46
+      imp13 <- 0
+      imp2 <- -1.11
+      imp6 <- 0
+    }
+    else if (input$radio == "5"){
+      imp1 <- -0.12
+      imp13 <- 0
+      imp2 <- -0.16
+      imp6 <- 0
+    }
+    else if (input$radio == "6"){
+      imp1 <- 0
+      imp13 <- 0
+      imp2 <- -100/20.46/20.46
+      imp6 <- -100/6.081^6.754
+      impe <- 0
+    }
+    else {
+      imp1 <- 0
+      imp13 <- 0
+      imp2 <- 0
+      imp6 <- 0
+    }
     
     SCC <- 0
     
@@ -140,15 +182,12 @@ server <- function(input, output) {
       RadForc[t] <- RFpar*log(CO2conc[t]/CO2pre)
       TempAtm[t] <- TempAtm[t-1] + l1*(l2*RadForc[t]-TempAtm[t-1]) + l3*(TempOc[t-1]-TempAtm[t-1])
       TempOc[t] <- TempOc[t-1] + l4*(TempAtm[t-1]-TempOc[t-1])
-      RadForc0[t] <- RFpar0*log(CO2conc[t]/CO2pre)
-      TempAtm0[t] <- TempAtm0[t-1] + l1*(l2*RadForc0[t]-TempAtm0[t-1]) + l3*(TempOc0[t-1]-TempAtm0[t-1])
-      TempOc0[t] <- TempOc0[t-1] + l4*(TempAtm0[t-1]-TempOc0[t-1])
-      Impact[t] <- (alpha*TempAtm[t] + beta*TempAtm[t]^2)*(income[t]/income[NHistYear])^input$IncElas
+      Impact[t] <- (imp1*TempAtm[t] + imp13*abs(TempAtm[t])^1.3 + imp2*TempAtm[t]^2 + imp6*abs(TempAtmP[t])^6.754)*(income[t]/income[NHistYear])^input$IncElas
       CO2concP[t] <- sum(CO2boxP[t,1:NCO2Box])
       RadForcP[t] <- RFpar*log(CO2concP[t]/CO2pre)
       TempAtmP[t] <- TempAtmP[t-1] + l1*(l2*RadForcP[t]-TempAtmP[t-1]) + l3*(TempOcP[t-1]-TempAtmP[t-1])
       TempOcP[t] <- TempOcP[t-1] + l4*(TempAtmP[t-1]-TempOcP[t-1])
-      ImpactP[t] <- (alpha*TempAtmP[t] + beta*TempAtmP[t]^2)*(income[t]/income[NHistYear])^input$IncElas
+      ImpactP[t] <- (imp1*TempAtmP[t] + imp13*abs(TempAtmP[t])^1.3 + imp2*TempAtmP[t]^2 + imp6*abs(TempAtmP[t])^6.754)*(income[t]/income[NHistYear])^input$IncElas
       if (t <= NHistYear){
         DF <- 0
       }
@@ -156,7 +195,7 @@ server <- function(input, output) {
         DF <- 1
       }
       else {
-        DF <- DF/(1+input$DiscRate/100)
+        DF <- DF/(1 + popgr + input$eta*incgr + input$PRTP/100)
       }
       SCC <- SCC + (Impact[t] - ImpactP[t])*income[t]*population[t]*DF
     }
@@ -172,42 +211,59 @@ server <- function(input, output) {
       co2hist[t] <- population[t]*income[t]*energyint[t]*carbonint[t]/1000000000
     }
     
-    resid <- TempAtm - Temperature
-    resid0 <- TempAtm0 - Temperature
-    RSS <- 0
-    RSS0 <- 0
-    mtemp <- 0
-    mtsq <- 0
-    n <- 0
-    for (t in 101:NHistYear){
-      n <- n+1
-      RSS <- RSS + (resid[t]-0.3)^2
-      RSS0 <- RSS0 + (resid0[t]-0.3)^2
-      mtemp <- mtemp + Temperature[t]-0.3
-      mtsq <- mtsq + (Temperature[t]-0.3)^2
-    }
-    mtemp <- mtemp/n
-    mtsq <- mtsq/n
-    vartemp <- mtsq-mtemp*mtemp
-    p <- exp(-0.5*RSS/vartemp)
-    p0 <- exp(-0.5*RSS0/vartemp)
-    ptmp <- matrix(p/p0,NYear,1)
     SCCv <- matrix(SCC,NYear,1)
-    #                     1     2          3       4         5           6          7       8          9          10  11     12   13  14     15        16         17     18     19        20    21                
-    resultsout <- cbind(Year, CO2emit, CO2conc, TempAtm, Temperature, population, income, energyint, carbonint, ppop, pinc, pei, pci, ptmp, pophist, inchist, eihist, cihist, co2hist, ImpactP, SCCv)
+    #                     1     2          3       4         5           6          7       8          9        10        11      12      13      14        15      16                
+    resultsout <- cbind(Year, CO2emit, CO2conc, TempAtm, Temperature, population, income, energyint, carbonint, pophist, inchist, eihist, cihist, co2hist, ImpactP, SCCv)
   })
   
     resultsimp <- reactive({
       impdata <- read.csv(file="totalimpact.csv", header=FALSE, sep=",")
       temp <- impdata[1:27,1]
       impobs <- impdata[1:27,2]
-      beta <- input$Benchmark/(2.5*2.5-2*2.5*input$Topt)
-      alpha <- -input$Topt*2*beta
+      if (input$radio == "1") {
+        imp1 <- 0
+        imp13 <- -1.0/2.5^.13
+        imp2 <- 0
+        imp6 <- 0
+      }
+      else if (input$radio == "2") {
+        imp1 <- 0
+        imp13 <- 0
+        imp2 <- -1.0/3/3
+        imp6 <- 0
+      }
+      else if (input$radio == "3") {
+        imp1 <- 0
+        imp13 <- 0
+        imp2 <- -2.5/3/3
+        imp6 <- 0
+      }
+      else if (input$radio == "4"){
+        imp1 <- 2.46
+        imp13 <- 0
+        imp2 <- -1.11
+        imp6 <- 0
+      }
+      else if (input$radio == "5"){
+        imp1 <- -0.12
+        imp13 <- 0
+        imp2 <- -0.16
+        imp6 <- 0
+      }
+      else if (input$radio == "6"){
+        imp1 <- 0
+        imp13 <- 0
+        imp2 <- -100/20.46/20.46
+        imp6 <- -100/6.081^6.754
+      }
+      else {
+        imp1 <- 0
+        imp13 <- 0
+        imp2 <- 0
+        imp6 <- 0
+      }
       impmod <- matrix(NA,27,1)
       impmod1 <- matrix(NA,27,1)
-      impbest <- matrix(NA,27,1)
-      res <- matrix(NA,27,1)
-      res0 <- matrix(NA,27,1)
       tempgrid <- matrix(NA,27,1)
       tempgrid[1] <- -1
       for (i in 2:27){
@@ -215,31 +271,10 @@ server <- function(input, output) {
       }
       
       for (i in 1:27){
-        impmod[i] <- alpha*tempgrid[i] + beta*tempgrid[i]^2
-        impmod1[i] <- alpha*temp[i] + beta*temp[i]^2
-        impbest[i] <- -0.12496*temp[i] -0.16442*temp[i]^2
-        res[i] <- impobs[i] - impmod1[i]
-        res0[i] <- impobs[i] - impbest[i]
+        impmod[i] <- imp1*tempgrid[i] + imp13*abs(tempgrid[i])^1.3 + imp2*tempgrid[i]^2 + imp6*abs(tempgrid[i])^6.754
+        impmod1[i] <- imp1*temp[i] + imp13*abs(tempgrid[i])^1.3 + imp2*temp[i]^2 + imp6*abs(temp[i])^6.754
       }
-      
-      rss <- 0
-      rss0 <- 0
-      mimp <- 0
-      misq <- 0
-      for (i in 1:27){
-        rss <- rss + res[i]^2
-        rss0 <- rss0 + res0[i]^2
-        mimp <- mimp + impobs[i]
-        misq <- misq + impobs[i]^2
-      }
-      mimp <- mimp/27
-      misq <- misq/27
-      varimp <- misq-mimp*mimp
-      pi <- exp(-0.5*rss/varimp)
-      pi0 <- exp(-0.5*rss0/varimp)
-      pimp <- matrix(pi/pi0,27,1)
-      
-      resultsimp <- cbind(temp,impobs,tempgrid,impmod,pimp)  
+      resultsimp <- cbind(temp,impobs,tempgrid,impmod)  
     })
     
     resultsco2 <- reactive({
@@ -249,7 +284,7 @@ server <- function(input, output) {
     output$plotI <- renderPlot({
       plot(resultsimp()[1:27,1],resultsimp()[1:27,2], xlab = 'degree Celsius', ylab = 'welfare equivalent income loss')
       lines(resultsimp()[1:27,3],resultsimp()[1:27,4], type="l", lty="solid")
-      title(paste("Impact of climate change. Likelihood ratio:",format(resultsimp()[1,5],digits=6)))
+      title(paste("Impact of climate change"))
     })
 
     output$plotT <- renderPlot({
@@ -257,51 +292,51 @@ server <- function(input, output) {
       plot(resultsout()[1:NYear,1],resultsout()[1:NYear,4], type = "l", xlab = 'year', ylab = 'degree Celsius',ylim = c(-0.5,6.0))
       lines(resultsout()[1:NHistYear,1],resultsout()[1:NHistYear,5]+0.3, type="p")
       lines(resultsout()[1:NYear,1],paris, type="l", lty="dashed")
-      title(paste("Temperature. Likelihood ratio:", format(resultsout()[NYear,10]*resultsout()[NYear,11]*resultsout()[NYear,12]*resultsout()[NYear,13]*resultsout()[NYear,14],digits=6)))
+      title(paste("Temperature"))
     })
     
     output$plotE <- renderPlot({
       plot(resultsout()[1:NYear,1],resultsout()[1:NYear,2]/1000, type = "l", xlab = 'year', ylab = 'billion tonnes of carbon per year')
-      lines(resultsout()[1:NHistYear,1],resultsout()[1:NHistYear,19]/1000, type="p")
-      title(paste("Carbon dioxide emissions. Likelihood ratio:",format(resultsout()[NYear,10]*resultsout()[NYear,11]*resultsout()[NYear,12]*resultsout()[NYear,13],digits=6)))
+      lines(resultsout()[1:NHistYear,1],resultsout()[1:NHistYear,14]/1000, type="p")
+      title(paste("Carbon dioxide emissions"))
     })
     
     output$plotP <- renderPlot({
       plot(resultsout()[1:NYear,1],resultsout()[1:NYear,6]/1000000000, type = "l", xlab = 'year', ylab = 'billion people')
-      lines(resultsout()[1:NHistYear,1],resultsout()[1:NHistYear,15]/1000000000, type="p")
-      title(paste("Population. Likelihood ratio:",format(resultsout()[NYear,10],digits=6)))
+      lines(resultsout()[1:NHistYear,1],resultsout()[1:NHistYear,10]/1000000000, type="p")
+      title(paste("Population"))
     })
     
     output$plotY <- renderPlot({
       plot(resultsout()[1:NYear,1],resultsout()[1:NYear,7], type = "l", xlab = 'year', ylab = 'dollar (2010) per person per year')
-      lines(resultsout()[1:NHistYear,1],resultsout()[1:NHistYear,16], type="p")
-      title(paste("Per capita income in 2100. Likelihood ratio:",format(resultsout()[NYear,11],digits=6)))
+      lines(resultsout()[1:NHistYear,1],resultsout()[1:NHistYear,11], type="p")
+      title(paste("Per capita income"))
     })
     
     output$plotEI <- renderPlot({
       plot(resultsout()[1:NYear,1],resultsout()[1:NYear,8]*1000, type = "l", xlab = 'year', ylab = 'gram oil equivalent per dollar', ylim = c(0,300))
-      lines(resultsout()[1:NHistYear,1],resultsout()[1:NHistYear,17]*1000, type="p")
-      title(paste("Energy intensity. Likelihood ratio:",format(resultsout()[NYear,12],digits=6)))
+      lines(resultsout()[1:NHistYear,1],resultsout()[1:NHistYear,12]*1000, type="p")
+      title(paste("Energy intensity"))
     })
 
     output$plotCI <- renderPlot({
       plot(resultsout()[1:NYear,1],resultsout()[1:NYear,9], type = "l", xlab = 'year', ylab = 'gram carbon per gram oil equivalent')
-      lines(resultsout()[1:NHistYear,1],resultsout()[1:NHistYear,18], type="p")
-      title(paste("Carbon intensity. Likelihood ratio:",format(resultsout()[NYear,13],digits=6)))
+      lines(resultsout()[1:NHistYear,1],resultsout()[1:NHistYear,13], type="p")
+      title(paste("Carbon intensity"))
     })
     
     output$plotC <- renderPlot({
       plot(resultsout()[1:NYear,1],resultsout()[1:NYear,3], type = "l", xlab = 'year', ylab = 'parts per million')
       lines(resultsco2()[,1],resultsco2()[,2], type="p")
-      title(paste("Carbon dioxide concentration. Likelihood ratio:",format(resultsout()[NYear,10]*resultsout()[NYear,11]*resultsout()[NYear,12]*resultsout()[NYear,13],digits=6)))
+      title(paste("Carbon dioxide concentration"))
     })
     
     output$plotD <- renderPlot({
-      plot(resultsout()[1:NYear,1],resultsout()[1:NYear,20], type = "l", xlab = 'year', ylab = 'percent income')
-      title(paste("Impact of climate change. Likelihood ratio:",format(resultsimp()[1,5]*resultsout()[NYear,10]*resultsout()[NYear,11]*resultsout()[NYear,12]*resultsout()[NYear,13]*resultsout()[NYear,14],digits=6)))
+      plot(resultsout()[1:NYear,1],resultsout()[1:NYear,15], type = "l", xlab = 'year', ylab = 'percent income')
+      title(paste("Impact of climate change"))
     })
     
-    output$SCC <- renderText({paste("Social cost of carbon: $",format(resultsout()[NYear,21]/1000000000,digits=4),"/tC")})
+    output$SCC <- renderText({paste("Social cost of carbon: $",format(resultsout()[NYear,16]/1000000000,digits=4),"/tC")})
 }
 
 # Run the application 
